fa7ee36366d5669a595dcb3eaea8796c357571f2
Gurpal Singh <gillgurpal699@gmail.com>
2022-12-05 10:36:39 +0000

    complete challenge 1 of day5

---
+++ day5/main.py
@@ -0,0 +1,46 @@
+from os import getcwd
+
+
+with open(f"{getcwd()}/input.txt", "r") as f:
+    lines = f.readlines()
+
+    stack = {"start": 0, "end": None}
+    instructions = {"start": None}
+
+    stacks = {}
+    for f in lines:
+        if f in ["\n", "\r\n"]:
+            instructions["start"] = lines.index(f)+1
+            stack["end"] = lines.index(f)-1
+
+            _stacks = lines[stack["end"]]
+            for index, s in enumerate(_stacks):
+                if s in [" ", "\n"]:
+                    continue
+                stacks[s] = []
+                for idx, l in enumerate(lines):
+                    if (idx == stack["end"]):
+                        break
+                    item = l[index]
+                    if item not in [" ", "]", "["]:
+                        stacks[s].append(item)
+            # print(stacks)
+            for l in lines[instructions["start"]:]:
+                if l not in ["\n", "\r\n"]:
+                    l = l.replace("\n", "").split()
+                    if (int(l[1]) == 1):
+                        stacks[f"{l[5]}"] = [
+                            stacks[f"{l[3]}"].pop(0), *stacks[f"{l[5]}"]]
+                        
+
+                    else:
+                        for _ in range(0, int(l[1])):
+                            stacks[f"{l[5]}"] = [
+                                stacks[f"{l[3]}"].pop(0), *stacks[f"{l[5]}"]]
+
+                        
+
+    required_sq =""
+    for k in stacks.keys():
+        required_sq += stacks[f"{k}"][0]
+    print(required_sq)
